07661056d6185874bd83a6f38fdb12ddb7437226
Pixiemix <ruthiecc360@gmail.com>
2015-07-20 09:31:59 +0100

    removed granularity from the data interface

--- src/data/interface.js
+++ src/data/interface.js
@@ -4,8 +4,7 @@
 	
 	fc .data.interface = function(){
 			var 	start = null, 
-			end = null, 
-			granularity = null;
+					end = null, 
 			
 			var params = [];
 				if (start != null) {
@@ -14,32 +13,8 @@
 				if (end != null) {
 					params.push("end=" + end.toISOString());
 				}
-				if (granularity != null) {
-					params.push("granularity=" + granularity.toISOString());
-				}
 	};
 
-   
-			
-			
 
 }(d3, fc));
 
-
-
-/*
-(function(d3, fc){
-	'use strict';
-	
-	fc.data.dataInterface
-		
-		
-	var params = [];
-	if (start != null){
-		params.push('start=' + start.toString());
-	}
-	if (end != null) {
-		params.push('end=' + end.toString());
-	}
-})
-*/
