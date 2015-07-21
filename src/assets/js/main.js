7ecfc0beafe6953d6cf5436a98082e8b34ef6396
Pixiemix <ruthiecc360@gmail.com>
2015-07-21 15:29:23 +0100

    working static data stream on simple d3 chart

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -8,35 +8,37 @@
         .append('svg')
         .attr('width', width)
         .attr('height', height);
-    var data = fc.data.coinbase();
 
-    /* fetch some data!
-    data(function(error, data) {
-        d3.select('#coinbase')
-        .text(JSON.stringify(error ? error : data, null, 2));
-    });*/
+    var data = fc.data.coinbase()
+        .start(new Date(2015, 4, 22))
+        .end(new Date(2015, 4, 30))
+        .granularity(10000);
 
-    //function renderChart(data) {
-    var chart = fc.chart.linearTimeSeries()
-        .xDomain(fc.util.extent(data, 'date'))
-        .yDomain(fc.util.extent(data, ['open', 'close']));
+    //var data = fc.data.stream();
+    var render = function(error, data) {
+        var chart = fc.chart.linearTimeSeries()
+            .xDomain(fc.util.extent(data, 'date'))
+            .yDomain(fc.util.extent(data, ['open', 'close']));
 
-    var area = fc.series.area()
-        .yValue(function(d) { return d.open; })
-        .y0Value(chart.yDomain()[0]);
+        var area = fc.series.area()
+            .yValue(function(d) { return d.open; })
+            .y0Value(chart.yDomain()[0]);
 
-    var line = fc.series.line()
-        .yValue(function(d) { return d.open; });
+        var line = fc.series.line()
+            .yValue(function(d) { return d.open; });
 
-    var gridlines = fc.annotation.gridline()
-        .yTicks(5)
-        .xTicks(0);
+        var gridlines = fc.annotation.gridline()
+            .yTicks(5)
+            .xTicks(0);
 
-    var multi = fc.series.multi()
-        .series([gridlines, area, line]);
+        var multi = fc.series.multi()
+            .series([gridlines, area, line]);
 
-    chart.plotArea(multi);
-    svg.datum(data)
-        .call(chart);
-    //}
+        chart.plotArea(multi);
+        svg.datum(data)
+            .call(chart);
+    };
+
+    data(render);
+
 })(d3, fc);
