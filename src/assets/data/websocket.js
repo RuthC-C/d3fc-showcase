830a98bfe8d7b94016f60919275c2b5de46ad859
unknown <rcaulcott@SL-WS-015.scottlogic.co.uk>
2015-07-24 11:59:36 +0100

    Added 2 different ways to get websocket. No luck with either.

--- src/assets/data/websocket.js
+++ src/assets/data/websocket.js
@@ -1,8 +1,49 @@
-(function(fc) {
+var ws;
+(function(ws) {
+    'use strict';
+    fc.data.websocket = function() {
+        if (window.WebSocker) {
+            console.log('Websocket object is support in browser');
+            ws = new WebSocket('wss://ws-feed.exchange.coinbase.com');
+            ws.onopen = function() {
+                console.log('onopen');
+            };
+            ws.onmessage = function(event) {
+                console.log('echo from serve : ' + event.data);
+            };
+
+            ws.onclose = function() {
+                console.log('onclose');
+            };
+            ws.onerror = function() {
+                console.log('onerror');
+            };
+
+        } else {
+            console.log('Websocket object not supported in your browser');
+        }
+        return ws;
+    };
+
+})(ws);
+
+
+
+
+/*(function(fc) {
     'use strict';
 
     fc.data.websocket = function() {
-        var coinbaseSocket = new WebSocket('wss://ws-feed.exchange.coinbase.com');
+        /*var coinbaseSocket = new WebSocket('wss://ws-feed.exchange.coinbase.com');
+
+        coinbaseSocket.onmessage = function(event) {
+            console.log(event.data);
+            return event.data;
+            //var msg = JSON.parse(event.data);
+            //var time = new Date(msg.date);
+        };*/
+
+        /*var coinbaseSocket = new WebSocket('wss://ws-feed.exchange.coinbase.com');
 
         coinbaseSocket.onmessage = function(event) {
             console.log(event.data);
@@ -10,9 +51,7 @@
             //var msg = JSON.parse(event.data);
             //var time = new Date(msg.date);
         };
-        return coinbaseSocket;
+        return coinbaseSocket.onmessage;
     };
 
-})(fc);
-
-
+})(fc);*/
